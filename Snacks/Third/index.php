45e68883e07a2a7c00a16dd601c9e7672ac23c14
Carlo1891 <carlo.18@live.it>
2022-08-23 17:28:21 +0200

    Third Snack begin

---
+++ Snacks/Third/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Terzo Array 15 numeri</title>
+    </head>
+
+    <body>
+        <!-- Creare un array con 15 numeri casuali, tenendo conto che l'array non dovrà contenere lo stesso numero più di una volta  -->
+
+        <?php
+
+        ?>
+        
+        
+    </body>
+</html>
