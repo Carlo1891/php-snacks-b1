0bd07f915284561a257b8447697d81849b24c0f6
Carlo1891 <carlo.18@live.it>
2022-08-23 16:41:52 +0200

    Second Snack begin

---
+++ Snacks/Second/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Secondo Iscrizione utente</title>
+    </head>
+
+    <body>
+        <h1>Registrazione necessaria</h1>
+        <!-- Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) 
+        che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. 
+        Se tutto è ok stampare "Accesso riuscito", altrimenti "Accesso negato" -->
+
+        <?php
+
+        ?>
+        
+        
+    </body>
+</html>
